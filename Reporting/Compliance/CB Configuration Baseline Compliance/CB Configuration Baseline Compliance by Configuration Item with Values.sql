0e606761fc9ad444aadbe96eb4a6c2fd647e7b1c
Ioan Popovici <me@ioan-popovici.ro>
2019-03-15 18:47:15 +0100

    Missed the release for this file somehow

--- Reporting/Compliance/CB Configuration Baseline Compliance/CB Configuration Baseline Compliance by Configuration Item with Values.sql
+++ Reporting/Compliance/CB Configuration Baseline Compliance/CB Configuration Baseline Compliance by Configuration Item with Values.sql
@@ -7,13 +7,13 @@
     Created by Ioan Popovici
     Requires SSRS/SQL, SCCM Configuration Baseline
 .LINK
-    BlogPost: https://sccm-zone.com/baseline-reporting-with-actual-values-output-in-sccm-73fec334ba8f
+    https://SCCM.Zone/CB-Configuration-Baseline-Compliance
 .LINK
-    Changes : https://SCCM.Zone/cb-configuration-baseline-compliance-changelog
+    https://SCCM.Zone/CB-Configuration-Baseline-Compliance-CHANGELOG
 .LINK
-    Github  : https://SCCM.Zone/cb-configuration-baseline-compliance
+    https://SCCM.Zone/CB-Configuration-Baseline-Compliance-GIT
 .LINK
-    Issues  : https://SCCM.Zone/issues
+    https://SCCM.Zone/Issues
 */
 
 /*##=============================================*/
@@ -21,36 +21,27 @@
 /*##=============================================*/
 
 /* Testing variables !! Need to be commented for Production !! */
---DECLARE @UserSIDs     AS NVARCHAR(250) = 1;
---DECLARE @CollectionID AS NVARCHAR(10)  = 'A01000EC';
---DECLARE @LocaleID     AS INT           = 2;
---DECLARE @BaselineID   AS INT           = 503286;
+--DECLARE @CollectionID AS NVARCHAR(10)  = 'HUB000B7';
+--DECLARE @LocaleID     AS INTEGER       = 2;
+--DECLARE @BaselineID   AS INTEGER       = 39225;
+
+/* Workaround for malformed UserSID */
+DECLARE @UserSIDs     AS NVARCHAR(250) = 1;
 
 /* Initialize CIID table */
 DECLARE @CIID TABLE
 (
-    CIID INT
-)
-
-/* Initialize SystemsInfo table */
-DECLARE @SystemsInfo TABLE
-(
-    ResourceID         INT
-    , DeviceName       NVARCHAR(250)
-    , OperatingSystem  NVARCHAR(250)
-    , OSVersion        NVARCHAR(250)
-    , Managed          NVARCHAR(5)
-    , ClientState      NVARCHAR(20)
+    CIID INTEGER
 )
 
 /* Initialize ComplianceInfo table */
 DECLARE @ComplianceInfo TABLE
 (
     ComplianceState  NVARCHAR(20)
-    , ResourceID     INT
+    , ResourceID     INTEGER
     , UserName       NVARCHAR(250)
-    , CIVersion      INT
-    , SettingVersion INT
+    , CIVersion      INTEGER
+    , SettingVersion INTEGER
     , SettingName    NVARCHAR(250)
     , RuleName       NVARCHAR(250)
     , Criteria       NVARCHAR(250)
@@ -63,62 +54,11 @@ DECLARE @ComplianceInfo TABLE
 /* Get CIs to process */
 INSERT INTO @CIID (CIID)
 SELECT ToCIID
-FROM dbo.fn_rbac_CIRelation(@UserSIDs)
+FROM dbo.v_CIRelation
 WHERE FromCIID = @BaselineID
-    AND RelationType NOT IN (7, 0) --Exlude itself and no relation
+    AND RelationType NOT IN (7, 0) -- Exlude itself and no relation
 
-/* Get systems data */
-INSERT INTO @SystemsInfo (ResourceID, Managed, ClientState, DeviceName, OperatingSystem, OSVersion)
-SELECT
-    ResourceID        = Computers.ResourceID
-    , Managed         =
-    CASE Computers.Client0
-        WHEN 1 THEN 'Yes'
-        ELSE 'No'
-    END
-    , ClientState     = ClientSummary.ClientStateDescription
-    , DeviceName      = Computers.Netbios_Name0
-    , OperatingSystem =
-        CASE
-            WHEN OperatingSystem.Caption0 != '' THEN
-                CONCAT(
-                    REPLACE(OperatingSystem.Caption0, 'Microsoft ', ''),         --Remove 'Microsoft ' from OperatingSystem
-                    REPLACE(OperatingSystem.CSDVersion0, 'Service Pack ', ' SP') --Replace 'Service Pack ' with ' SP' in OperatingSystem
-                )
-            ELSE
-
-            /* Workaround for systems not in GS_OPERATING_SYSTEM table */
-            (
-                CASE
-                    WHEN CombinedResources.DeviceOS LIKE '%Workstation 6.1%'    THEN 'Windows 7'
-                    WHEN CombinedResources.DeviceOS LIKE '%Workstation 6.2%'    THEN 'Windows 8'
-                    WHEN CombinedResources.DeviceOS LIKE '%Workstation 6.3%'    THEN 'Windows 8.1'
-                    WHEN CombinedResources.DeviceOS LIKE '%Workstation 10.0%'   THEN 'Windows 10'
-                    WHEN CombinedResources.DeviceOS LIKE '%Server 6.0'          THEN 'Windows Server 2008'
-                    WHEN CombinedResources.DeviceOS LIKE '%Server 6.1'          THEN 'Windows Server 2008R2'
-                    WHEN CombinedResources.DeviceOS LIKE '%Server 6.2'          THEN 'Windows Server 2012'
-                    WHEN CombinedResources.DeviceOS LIKE '%Server 6.3'          THEN 'Windows Server 2012 R2'
-                    WHEN CombinedResources.DeviceOS LIKE '%Server 10.0'         THEN 'Windows Server 2016'
-                    ELSE 'Unknown'
-                END
-            )
-        END
-    , OSVersion       =
-        (
-            SELECT OSLocalizedNames.Value
-            FROM fn_GetWindowsServicingLocalizedNames() AS OSLocalizedNames
-                INNER JOIN fn_GetWindowsServicingStates() AS OSServicingStates ON OSServicingStates.Build = Computers.Build01
-            WHERE OSLocalizedNames.Name = OSServicingStates.Name
-                AND Computers.OSBranch01 = OSServicingStates.branch --Select only the branch of the installed OS
-        )
-FROM fn_rbac_FullCollectionMembership(@UserSIDs) AS CollectionMembers
-    LEFT JOIN fn_rbac_R_System(@UserSIDs) AS Computers ON Computers.ResourceID = CollectionMembers.ResourceID
-    LEFT JOIN fn_rbac_GS_OPERATING_SYSTEM(@UserSIDs) OperatingSystem ON OperatingSystem.ResourceID = CollectionMembers.ResourceID
-    LEFT JOIN fn_rbac_CombinedDeviceResources(@UserSIDs) AS CombinedResources ON CombinedResources.MachineID = CollectionMembers.ResourceID
-    LEFT JOIN fn_rbac_CH_ClientSummary(@UserSIDs) AS ClientSummary ON ClientSummary.ResourceID = CollectionMembers.ResourceID
-WHERE CollectionMembers.CollectionID = @CollectionID
-
-/* Get compliance data */
+/* Get general compliance data  and add it to ComplianceInfo table */
 INSERT INTO @ComplianceInfo (ResourceID, ComplianceState, UserName, CIVersion, SettingVersion, SettingName, RuleName, Criteria, ActualValue, InstanceData, LastEvaluation, Severity)
 SELECT DISTINCT
     ResourceID        = CISettingsStatus.ResourceID
@@ -132,7 +72,7 @@ SELECT DISTINCT
     , ActualValue     = CISettingsStatus.CurrentValue
     , InstanceData    = CISettingsStatus.InstanceData
     , LastEvaluation  = CISettingsStatus.LastComplianceMessageTime
-    , Severity        =
+    , Severity        = (
         CASE CISettingsStatus.RuleSeverity
             WHEN 0 THEN 'None'
             WHEN 1 THEN 'Information'
@@ -140,17 +80,18 @@ SELECT DISTINCT
             WHEN 3 THEN 'Critical'
             WHEN 4 THEN 'Critical with event'
         END
-FROM fn_rbac_FullCollectionMembership(@UserSIDs) AS CollectionMembers
-    INNER JOIN fn_rbac_CIComplianceStatusDetail(@UserSIDs) AS CISettingsStatus ON CISettingsStatus.ResourceID = CollectionMembers.ResourceID
+    )
+FROM v_FullCollectionMembership AS CollectionMembers
+    INNER JOIN v_CIComplianceStatusDetail AS CISettingsStatus ON CISettingsStatus.ResourceID = CollectionMembers.ResourceID
     INNER JOIN fn_ListCIRules(@UserSIDs) AS CIRules ON CIRules.Rule_UniqueID = CISettingsStatus.Rule_UniqueID
-        AND CIRules.CIVersion = CISettingsStatus.CIVersion --Select only curent baseline version
+        AND CIRules.CIVersion = CISettingsStatus.CIVersion -- Select only curent baseline version
     INNER JOIN fn_ListCISettings(@LocaleID) AS CISettings ON CISettings.Setting_UniqueID = CISettingsStatus.Setting_UniqueID
         AND CISettings.CIVersion = CISettingsStatus.CIVersion
     INNER JOIN fn_rbac_ListCI_ComplianceState(@LocaleID, @UserSIDs) AS CIComplianceState ON CIComplianceState.ResourceID = CollectionMembers.ResourceID
         AND CIComplianceState.CI_ID = @BaselineID
 WHERE CollectionMembers.CollectionID = @CollectionID
     AND CISettingsStatus.CI_ID IN (SELECT CIID FROM @CIID)
-	AND CIComplianceState.ComplianceStateName != 'Error' --We are adding errors below, removing artefacts if any
+    AND CIComplianceState.ComplianceStateName != 'Error'   -- We are adding errors below, removing artefacts if any
 
 /* Get error data and add it to ComplianceInfo table */
 INSERT INTO @ComplianceInfo (ResourceID, ComplianceState, UserName, CIVersion, SettingVersion, SettingName, RuleName, Criteria, ActualValue, InstanceData)
@@ -165,47 +106,75 @@ SELECT
     , Criteria        = ErrorDetails.ObjectTypeName
     , ActualValue     = ErrorDetails.ErrorTypeDisplay
     , InstanceData    = ErrorDetails.ErrorCode
-FROM fn_rbac_FullCollectionMembership(@UserSIDs) AS CollectionMembers
+FROM v_FullCollectionMembership AS CollectionMembers
 INNER JOIN fn_DCMDeploymentErrorDetailsPerAsset(@LocaleID) AS ErrorDetails ON ErrorDetails.AssetID = CollectionMembers.ResourceID
     AND ErrorDetails.BL_ID = @BaselineID
 WHERE CollectionMembers.CollectionID = @CollectionID
 
-/* Join SystemsInfo and ComplianceInfo data */
+/* Get compliant data and add it to ComplianceInfo table */
+INSERT INTO @ComplianceInfo (ResourceID, ComplianceState, UserName, CIVersion, SettingVersion, SettingName, RuleName, Criteria, ActualValue, InstanceData)
 SELECT
-    ComplianceState   = ( SELECT ISNULL(ComplianceInfo.ComplianceState, 'Unknown' ))
-    , Managed         = SystemsInfo.Managed
-    , ClientState     = SystemsInfo.ClientState
-    , DeviceName      = SystemsInfo.DeviceName
-    , OperatingSystem = CONCAT(SystemsInfo.OperatingSystem, (' ' + SystemsInfo.OSVersion))
-    , UserName        = ComplianceInfo.UserName
-    , CIVersion       = ComplianceInfo.CIVersion
-    , SettingVersion  = ComplianceInfo.SettingVersion
-    , SettingName     = ComplianceInfo.SettingName
-    , RuleName        = ComplianceInfo.RuleName
-    , Criteria        = ComplianceInfo.Criteria
-    , ActualValue     = ComplianceInfo.ActualValue
-    , InstanceData    = ComplianceInfo.InstanceData
-    , LastEvaluation  = ComplianceInfo.LastEvaluation
-    , Severity        = ComplianceInfo.Severity
-FROM @SystemsInfo AS SystemsInfo
-    LEFT JOIN @ComplianceInfo AS ComplianceInfo ON ComplianceInfo.ResourceID = SystemsInfo.ResourceID
-WHERE DeviceName != '' --Eliminate artefacts with no device name
-ORDER BY
-    ComplianceState
-    , Managed
-    , ClientState
-    , DeviceName
-    , OperatingSystem
-    , UserName
-    , CIVersion
-    , SettingVersion
-    , SettingName
-    , RuleName
-    , Criteria
-    , ActualValue
-    , InstanceData
-    , LastEvaluation
-    , Severity
+    ResourceID        = CompliantDetails.AssetID
+    , ComplianceState = 'Compliant'
+    , UserName        = CompliantDetails.ADUserName
+    , CIVersion       = CompliantDetails.BLRevision
+    , SettingVersion  = CompliantDetails.Revision
+    , SettigName      = CompliantDetails.CIName
+    , RuleName        = CompliantDetails.RuleName
+    , Criteria        = CompliantDetails.ValidationRule
+    , ActualValue     = CompliantDetails.DiscoveredValue
+    , InstanceData    = CompliantDetails.InstanceData
+FROM v_FullCollectionMembership AS CollectionMembers
+INNER JOIN fn_DCMDeploymentCompliantDetailsPerAsset(@LocaleID) AS CompliantDetails ON CompliantDetails.AssetID = CollectionMembers.ResourceID
+    AND CompliantDetails.BL_ID = @BaselineID
+WHERE CollectionMembers.CollectionID = @CollectionID;
+
+/* Get systems data and join ComplianceInfo table */
+    SELECT
+        ComplianceState   = ISNULL(ComplianceInfo.ComplianceState, 'Unknown')
+        , Managed         = (
+            CASE Systems.Client0
+                WHEN 1 THEN 'Yes'
+                ELSE 'No'
+            END
+        )
+        , ClientState     = ClientSummary.ClientStateDescription
+        , DeviceName      = Systems.Netbios_Name0
+        , OperatingSystem = (
+            CASE
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Workstation 10.0%' THEN 'Windows 10'
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Server 10%'        THEN (
+                    CASE
+                        WHEN CAST(REPLACE(Systems.Build01, '.', '') AS INTEGER) > 10017763             THEN 'Windows Server 2019'
+                        ELSE 'Windows Server 2016'
+                    END
+                )
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Server 6.3'        THEN 'Windows Server 2012 R2'
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Server 6.2'        THEN 'Windows Server 2012'
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Server 6.1'        THEN 'Windows Server 2008R2'
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Server 6.0'        THEN 'Windows Server 2008'
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Workstation 6.3%'  THEN 'Windows 8.1'
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Workstation 6.2%'  THEN 'Windows 8'
+                WHEN Systems.Operating_System_Name_and0 LIKE 'Microsoft Windows NT %Workstation 6.1%'  THEN 'Windows 7'
+                ELSE 'Unknown'
+            END
+        )
+        , UserName        = ComplianceInfo.UserName
+        , CIVersion       = ComplianceInfo.CIVersion
+        , SettingVersion  = ComplianceInfo.SettingVersion
+        , SettingName     = ComplianceInfo.SettingName
+        , RuleName        = ComplianceInfo.RuleName
+        , Criteria        = ComplianceInfo.Criteria
+        , ActualValue     = ComplianceInfo.ActualValue
+        , InstanceData    = ComplianceInfo.InstanceData
+        , LastEvaluation  = ComplianceInfo.LastEvaluation
+        , Severity        = ComplianceInfo.Severity
+    FROM v_FullCollectionMembership AS CollectionMembers
+        LEFT JOIN v_R_System AS Systems ON Systems.ResourceID = CollectionMembers.ResourceID
+        LEFT JOIN v_CH_ClientSummary AS ClientSummary ON ClientSummary.ResourceID = CollectionMembers.ResourceID
+        LEFT JOIN @ComplianceInfo AS ComplianceInfo ON ComplianceInfo.ResourceID = CollectionMembers.ResourceID
+    WHERE CollectionMembers.CollectionID = @CollectionID
+        AND Systems.Netbios_Name0 !=  '' -- Eliminate artefacts with no device name
 
 /*##=============================================*/
 /*## END QUERY BODY                              */
